e625472c6faa3adb2b13463a31049d09a7cb578e
Michael Anderson <mjanders6@gmail.com>
2019-03-24 02:30:05 -0700

    updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,23 +11,49 @@ selArr.forEach(
         document.querySelector('#bttns').append(btnElem)
     })
 
+let toggle = false;
+
 document.addEventListener('click', ({ target }) => {
     document.querySelector('#pics').innerHTML = ''
-
-    let animal = target.className
-    fetch(`http://api.giphy.com/v1/gifs/search?q=${animal}&rating=g&api_key=OeBLbdQVfVJi0hB3KDlP2IdhsDjmQetJ&limit=10`)
+if (target.id === 'btn') {
+    let pic = target.className
+    fetch(`http://api.giphy.com/v1/gifs/search?q=${pic}&rating=g&api_key=OeBLbdQVfVJi0hB3KDlP2IdhsDjmQetJ&limit=10`)
         .then(r => r.json())
         .then(({ data }) => {
+            
             data.forEach(pic => {
                 const url = pic.images.fixed_height.url
+                const url_stat = pic.images.fixed_height_still.url
                 let picElem = document.createElement('img')
+                let hElem = document.createElement('div')
+                
+                // set attributes for the images
+                picElem.setAttribute('id', 'gifPic')
                 picElem.setAttribute('src', url)
-                document.querySelector('#pics').append(picElem)
-                console.log(pic);
+                picElem.setAttribute('data-static', url_stat)
+                picElem.setAttribute('data-animated', url)
 
+                hElem.innerHTML = `<h3>${pic.rating}</h3>`
+
+                document.querySelector('#pics').append(picElem)
             })
+
         })
-        .catch(e => console.error(e))
+
+} else if (target.id === 'gifPic') {
+    console.log(target);
+    
+    toggle = !toggle
+    if (toggle) {
+        target.setAttribute('src', target.dataset.animated)
+        
+    } else {
+        target.setAttribute('src', target.dataset.static)
+
+    }
+    
+}
+
 
 
 })
