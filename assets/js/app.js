1dbbc39081784239a067f588fa34c5423755e383
Michael Anderson <mjanders6@gmail.com>
2019-03-24 14:14:22 -0700

    added flex containers to make the pics wrap

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,32 +21,9 @@ let addBtn = () => {
     
 }
 
-let picArry = (target) => {
-    let pic = target.className
-    fetch(`http://api.giphy.com/v1/gifs/search?q=${pic}&rating=g&api_key=OeBLbdQVfVJi0hB3KDlP2IdhsDjmQetJ&limit=10`)
-        .then(r => r.json())
-        .then(({ data }) => {
-    
-            data.forEach(pic => {
-                let url = pic.images.fixed_height.url
-                let url_stat = pic.images.fixed_height_still.url
-                let docElem = document.createElement('div')
-    
-                docElem.innerHTML = `
-            <h3>${pic.rating}</h3>
-            <img id="gifPic" src="${url}" data-static="${url_stat}" data-animated="${url}" data-toggle="${toggle}">
-            `
-                document.querySelector('#pics').append(docElem)
-            })
-    
-        })
-
-}
-// src="${url}"
 arry()
 
 let toggle = false;
-let url, url_stat
 document.addEventListener('click', ({ target }) => {
 
     if (target.id === 'btn') {
@@ -60,16 +37,16 @@ document.addEventListener('click', ({ target }) => {
                     let url = pic.images.fixed_height.url
                     let url_stat = pic.images.fixed_height_still.url
                     let docElem = document.createElement('div')
+                    docElem.id = 'picDiv-item-item'
         
                     docElem.innerHTML = `
-                <h3>${pic.rating}</h3>
-                <img id="gifPic" src="${url}" data-static="${url_stat}" data-animated="${url}" data-toggle="${toggle}">
+                <h3>Rating: ${pic.rating}</h3>
+                <img id="gifPic" src="${url_stat}" data-static="${url_stat}" data-animated="${url}" data-toggle="${toggle}">
                 `
                     document.querySelector('#pics').append(docElem)
                 })
-        
             })
-
+// I couldnt think of a name other than toggle
     } else if (target.id === 'gifPic') {
         toggle = !toggle
 
@@ -79,13 +56,7 @@ document.addEventListener('click', ({ target }) => {
             
         } else {
             target.setAttribute('src', target.dataset.static)
-            
-        }
-
-        
+        }  
     }
-    
-
-
 })
 
